51bef4129dd0cf678c05165f09f7a53f7f3ec00b
alejandra cantu <alejandra.cantu13@gmail.com>
2020-12-27 05:52:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -31,11 +31,13 @@ Show.sum("rating")
   def popular_shows
     #returns an array of all of the shows that have a rating greater than 5
     #hint: use the where Active Record method
+    Show.where('rating > 5')
   end
 
   def shows_by_alphabetical_order
     #returns an array of all of the shows sorted by alphabetical
     #order according to their names. hint: use the order Active Record method.
+    Show.order(name: :asc)
   end
 
 end
